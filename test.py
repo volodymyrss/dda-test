56c32829ac10cc424a6f53f38aa37e92c716c0d0
Volodymyr <volodymyrss@users.noreply.github.com>
2018-10-14 13:20:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 print("dda module for test import")
 
+import os
 import dataanalysis.core as da
 #import ddaontology as do
 
 class TestNode(da.DataAnalysis):
     def main(self):
         self.data={'test':'data'}
-        with open("data-file.txt","w") as f:
+        wd=os.getcwd()
+        with open(wd+"/data-file.txt","w") as f:
             f.write("data")
         self.data_file=da.DataFile("data-file.txt")
     
